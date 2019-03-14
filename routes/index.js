666b86fd3ccd825e73bb8734e3ecdf0bea7bd8c0
blake <blakehause@Blakes-MBP.fc.ca>
2019-03-14 17:38:59 -0400

    Create routes for user1 and user2, having content dynamically change

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const data = require('../data');
+
 /* GET home page. */
 // get the rout and request to render the index with the title express
 // index actually is index.hbs and the data that goes into it is coming from the route
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Your first Express App', message: 'Express is actually pretty easy once you figure a few things out.'});
+});
+
+// get a dynamic user via the route parameter
+router.get('/:user', function(req, res, next) {
+  console.log('user', req.params.user);
+  res.render('userpage', data[req.params.user]);
 });
 
 module.exports = router;
