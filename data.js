50f2f7a72fd254782eda8a7db4885d7553f363b1
blake <blakehause@Blakes-MacBook-Pro.local>
2019-03-21 22:42:06 -0400

    playing with mustaches

--- data.js
+++ data.js
@@ -3,14 +3,14 @@ const data = {
     role: "Student",
 
     product1: {
-        name: "product 1",
+        name: "The Product",
         role: "The Only One Night Stand",
         bio: "some generic bio text",
         bioPic: "arrival.jpg"
     },
 
     product2: {
-        name: "user2",
+        name: "The Details.",
         role: "How this one night stand works.",
         bio: "some generic bio text for user 2",
         bioPic: "logan.jpg"
