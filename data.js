eec00cdf2cef88dd600c09b1e78a4b090813c21b
blake <blakehause@Blakes-MacBook-Pro.local>
2019-03-22 23:48:53 -0400

    completed the styling and text

--- data.js
+++ data.js
@@ -3,17 +3,17 @@ const data = {
     role: "Student",
 
     product1: {
-        name: "the most intelligent night stand.",
-        role: "The Only One Night Stand",
-        bio: "some generic bio text",
+        name: "the most intelligent night stand",
+        role: " most intelligent night stand designed to date.",
+        bio: "with built in bluetooth speakers, charging ports/ plug-ins and a controlable light bar this night stand the perfect accent to any room.",
         bioPic: "nightstandLight.jpg"
     },
 
     product2: {
-        name: "The Details.",
-        role: "How this one night stand works.",
-        bio: "some generic bio text for user 2",
-        bioPic: "nightstandcolor.jpg"
+        name: "our color variations",
+        role: " black and white night stands.",
+        bio: "Both the black and white colour variations provide a beautiful modern touch to any room",
+        bioPic: "nightstandcolors.jpg"
     }
 }
 
